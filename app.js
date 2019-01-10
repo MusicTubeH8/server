bde57a6f32b65fc2faf2ad633613629dd5b02def
Ribka Haryuni <ribka.blue@gmail.com>
2019-01-10 22:25:50 +0700

    add video interface

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var lyricsRouter = require('./routes/lyrics');
 var topchartRouter = require('./routes/topchart');
+var youtubeRouter = require('./routes/youtube');
 
 var app = express();
 
@@ -23,5 +24,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/lyrics', lyricsRouter);
 app.use('/topchart', topchartRouter);
+app.use('/youtube', youtubeRouter);
 
 module.exports = app;
