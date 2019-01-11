c6b8102f55046a44eff4cd52ac6aaf83b1901877
ChristianHS23 <christian.sihotang23@gmail.com>
2019-01-11 11:26:06 +0700

    Made changes

--- controllers/topchart.js
+++ controllers/topchart.js
@@ -4,7 +4,7 @@ const axios = require('axios')
 class TopChartController {
     //mencari top chart lagu global
     static getTopTracks(req, res) {
-        axios.get('http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=e63c4bed1b8261946762b12f7a31913b&format=json')
+        axios.get('http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&limit=5&api_key=e63c4bed1b8261946762b12f7a31913b&format=json')
             .then(response => {
                 res.json(response.data.tracks)
             })
@@ -18,7 +18,7 @@ class TopChartController {
     //mencari top chart lagu pada sebuah negara
     static getTopTracksCountry(req, res) {
         let country = req.params.country
-        axios.get(`http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=${country}&api_key=e63c4bed1b8261946762b12f7a31913b&format=json`)
+        axios.get(`http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&limit=5&country=${country}&api_key=e63c4bed1b8261946762b12f7a31913b&format=json`)
             .then(response => {
                 res.json(response.data.tracks)
             })
